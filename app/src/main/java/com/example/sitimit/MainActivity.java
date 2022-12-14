4e69eec1172b3508e0f4f2529241e35368efa9d5
mochandri <72057874+mochandri@users.noreply.github.com>
2022-12-14 16:20:00 +0700

    kategori produk

--- app/src/main/java/com/example/sitimit/MainActivity.java
+++ app/src/main/java/com/example/sitimit/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -44,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+//    public void backHome(){
+//        Intent intent =new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//
+//    }
 
         }
 
