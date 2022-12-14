3d4228478a16d5ca30ea433ccc2b4c5911ff3108
mochandri <72057874+mochandri@users.noreply.github.com>
2022-12-14 10:42:48 +0700

    lanjutan

--- app/src/main/java/com/example/sitimit/MainActivity.java
+++ app/src/main/java/com/example/sitimit/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.sitimit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null ;
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_kalkulator:
+                        selectedFragment = new KalkulatorFragment();
+                        break;
+                    case R.id.nav_produk:
+                        selectedFragment = new ProdukFragment();
+                        break;
+                    case R.id.nav_tentangKami:
+                        selectedFragment = new TentangKamiFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            }
+        });
     }
 }
