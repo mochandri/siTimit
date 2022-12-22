a4074350b2ff483d1f575335328d186a850f471e
FajarAbadi <fajarfitra111@gmail.com>
2022-12-22 15:29:24 +0700

    spinner dropdown tebal bata

--- app/src/main/java/com/example/sitimit/KalkulatorBata.java
+++ app/src/main/java/com/example/sitimit/KalkulatorBata.java
@@ -3,8 +3,11 @@ package com.example.sitimit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class KalkulatorBata extends AppCompatActivity {
     Spinner spinner;
@@ -16,7 +19,21 @@ public class KalkulatorBata extends AppCompatActivity {
         setContentView(R.layout.activity_kalkulator_bata);
         spinner=findViewById(R.id.spiner );
 
-        ArrayAdapter<String> adapter= new ArrayAdapter<String>(KalkulatorBata.this, android.R.layout.simple_spinner_item,tebalbata);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(KalkulatorBata.this, R.layout.item_file,tebalbata);
+        adapter.setDropDownViewResource(R.layout.item_file);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value = parent.getItemAtPosition(position).toString();
+                Toast.makeText(KalkulatorBata.this, value, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
