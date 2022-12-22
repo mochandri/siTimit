a7b9d2ca3a130d03500818936fbab5b6c81d1729
FajarAbadi <fajarfitra111@gmail.com>
2022-12-22 15:17:01 +0700

    coba

--- app/src/main/java/com/example/sitimit/KalkulatorBata.java
+++ app/src/main/java/com/example/sitimit/KalkulatorBata.java
@@ -3,12 +3,20 @@ package com.example.sitimit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class KalkulatorBata extends AppCompatActivity {
+    Spinner spinner;
+    String[] tebalbata = {"7,5", "10", "12,5", "15", "17,5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalkulator_bata);
+        spinner=findViewById(R.id.spiner );
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(KalkulatorBata.this, android.R.layout.simple_spinner_item,tebalbata);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     }
 }
