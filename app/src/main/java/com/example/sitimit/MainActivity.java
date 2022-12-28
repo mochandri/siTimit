d62598b0cb71cbfc70bb0bfd0b9083e4c04c43f3
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2022-12-28 09:06:38 +0700

    perbaikn

--- app/src/main/java/com/example/sitimit/MainActivity.java
+++ app/src/main/java/com/example/sitimit/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
     private  void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.replace(com.denzcoskun.imageslider.R.id.linear_layout, fragment);
         fragmentTransaction.commit();
 
     }
