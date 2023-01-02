cf9634f42a9d3aff55002d6f83fb4918662d5687
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2023-01-02 13:36:43 +0700

    tiga mitra malang

--- app/src/main/java/com/example/sitimit/tigamitramalang.java
+++ app/src/main/java/com/example/sitimit/tigamitramalang.java
@@ -2,7 +2,10 @@ package com.example.sitimit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class tigamitramalang extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class tigamitramalang extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tigamitramalang);
+    }
+    public void btnwa(View view){
+        String url="https://api.whatsapp.com/send?phone=082132137447&text=Halo%20Bapak%20Taufik%20Tiga%20Mitra%20Malang,%20Saya%20Mau%20Bertanya%20Tentang%20Material%20Bangunan";
+        Intent bukawa = new Intent(Intent.ACTION_VIEW);
+        bukawa.setData(Uri.parse(url));
+        startActivity(bukawa);
+
     }
 }
