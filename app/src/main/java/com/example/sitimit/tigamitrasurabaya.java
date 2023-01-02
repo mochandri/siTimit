bf47d8cc0d65b9fa2f7ac7bd70a4d354a100c2ec
Fitri Ariska <72059490+FitriAriska@users.noreply.github.com>
2023-01-02 10:58:47 +0700

    btn WA hubungi kami

--- app/src/main/java/com/example/sitimit/tigamitrasurabaya.java
+++ app/src/main/java/com/example/sitimit/tigamitrasurabaya.java
@@ -2,7 +2,10 @@ package com.example.sitimit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class tigamitrasurabaya extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class tigamitrasurabaya extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tigamitrasurabaya);
+    }
+    public void btnwa(View view){
+        String url="https://wa.me/6281231313222";
+        Intent bukawa = new Intent(Intent.ACTION_VIEW);
+        bukawa.setData(Uri.parse(url));
+        startActivity(bukawa);
+
     }
 }
