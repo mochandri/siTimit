00e07e891c3cf86979dd54b2a2bf45ff0e99300b
mochandri <72057874+mochandri@users.noreply.github.com>
2023-01-03 22:34:26 +0700

    produk

--- app/src/main/java/com/example/sitimit/HomeFragment.java
+++ app/src/main/java/com/example/sitimit/HomeFragment.java
@@ -37,17 +37,6 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        back = view.findViewById(R.id.btnkategori);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment kategori = new ProdukFragment();
-                FragmentTransaction fm = getFragmentManager().beginTransaction();
-                fm.replace(R.id.kategori,kategori);
-                fm.addToBackStack(null);
-                fm.remove(kategori).commit();
-            }
-        });
 
         imageSlider = view.findViewById(R.id.image_slider);
 
