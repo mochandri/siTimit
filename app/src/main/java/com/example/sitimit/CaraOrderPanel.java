f6120630964176326b6039ebcdb4b43b65add768
FajarAbadi <fajarfitra111@gmail.com>
2023-01-07 23:47:30 +0700

    sek coba

---
+++ app/src/main/java/com/example/sitimit/CaraOrderPanel.java
@@ -0,0 +1,17 @@
+package com.example.sitimit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CaraOrderPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cara_order_panel);
+
+
+    }
+}
--- app/src/main/java/com/example/sitimit/ProdukFragment.java
+++ app/src/main/java/com/example/sitimit/ProdukFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,9 +17,6 @@ import android.widget.Button;
  * create an instance of this fragment.
  */
 public class ProdukFragment extends Fragment {
-
-
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
