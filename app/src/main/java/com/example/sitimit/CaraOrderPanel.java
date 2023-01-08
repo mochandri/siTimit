dc823e622e6505fe85255ec84fc58c394abaf939
FajarAbadi <fajarfitra111@gmail.com>
2023-01-08 12:14:45 +0700

    coba dilihat rek

--- app/src/main/java/com/example/sitimit/CaraOrderPanel.java
+++ app/src/main/java/com/example/sitimit/CaraOrderPanel.java
@@ -2,7 +2,11 @@ package com.example.sitimit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CaraOrderPanel extends AppCompatActivity {
@@ -10,6 +14,11 @@ public class CaraOrderPanel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cara_order_panel);
-
+    }
+    public void btnno(View view){
+        String url="https://wa.me/6281231313222";
+        Intent bukawa = new Intent(Intent.ACTION_VIEW);
+        bukawa.setData(Uri.parse(url));
+        startActivity(bukawa);
     }
 }
